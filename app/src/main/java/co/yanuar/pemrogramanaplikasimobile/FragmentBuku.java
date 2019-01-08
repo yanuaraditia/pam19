28245cdc6edaa1da2529b930050a45107b219b03
16110230 <achmad.0230@students.amikom.ac.id>
2019-01-08 19:18:17 +0700

    fragment java

---
+++ app/src/main/java/co/yanuar/pemrogramanaplikasimobile/FragmentBuku.java
@@ -0,0 +1,33 @@
+package co.yanuar.pemrogramanaplikasimobile;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentBuku extends Fragment {
+
+
+    public FragmentBuku() {}
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_entertainment, container, false);
+    }
+
+}
